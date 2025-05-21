44a0920f70f07ec4c694d87d3b9dedf838afe1a7
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-21 09:46:29 +0300

    Finished Creating the Footer Section and the landing page

--- client/src/app/(nandashboard)/landing/page.tsx
+++ client/src/app/(nandashboard)/landing/page.tsx
@@ -1,8 +1,16 @@
 import HeroSection from "@/app/(nandashboard)/landing/HeroSection";
+import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "@/app/(nandashboard)/landing/CallToActionSection";
+import FooterSection from "@/app/(nandashboard)/landing/FooterSection";
 
 const Landing = () => {
   return <div>
     <HeroSection />
+    <FeaturesSection />
+    <DiscoverSection />
+    <CallToActionSection/>
+    <FooterSection/>
   </div>;
 };
 
