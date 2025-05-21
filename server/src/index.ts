de7f1b76393526de30681db6212aff0fe7da1d84
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-21 11:08:18 +0300

    intiialized my back-end using Nodejs and express , installed the necessary packages and set up my server and home route

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// CONFIGURATION
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("This is the hme route");
+});
+
+//server
+
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
